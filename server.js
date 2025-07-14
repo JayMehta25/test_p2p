61128e6eabed837837b9da31ed7f7e65b1489479
Jay Mehta <jayjmehta251203@gmail.com>
2025-07-14 16:48:09 +0530

    Server changes for 2+ users

--- server.js
+++ server.js
@@ -6,31 +6,41 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+const userRooms = {};
+
 io.on('connection', socket => {
   console.log('A user connected:', socket.id);
 
   socket.on('join', room => {
     socket.join(room);
-    console.log(`User ${socket.id} joined room: ${room}`);
-    socket.to(room).emit('peer-joined');
+    userRooms[socket.id] = room;
+    // Send all other users in the room to the new user
+    const usersInRoom = Array.from(io.sockets.adapter.rooms.get(room) || []).filter(id => id !== socket.id);
+    console.log(`[JOIN] User ${socket.id} joined room: ${room}`);
+    console.log(`[JOIN] Users in room now:`, Array.from(io.sockets.adapter.rooms.get(room) || []));
+    console.log(`[JOIN] Emitting all-users to ${socket.id}:`, usersInRoom);
+    socket.emit('all-users', usersInRoom);
+    // Notify others in the room
+    socket.to(room).emit('user-joined', socket.id);
+    console.log(`[JOIN] Emitting user-joined to room ${room} (except ${socket.id})`);
     // Emit user count to all in the room
     const userCount = io.sockets.adapter.rooms.get(room)?.size || 0;
     io.to(room).emit('user-count', userCount);
   });
 
-  socket.on('signal', ({ room, data }) => {
-    console.log(`Signal from ${socket.id} to room ${room}:`, data.sdp ? 'SDP' : 'ICE Candidate');
-    socket.to(room).emit('signal', data);
+  socket.on('signal', ({ to, data }) => {
+    io.to(to).emit('signal', { from: socket.id, data });
   });
 
   socket.on('disconnecting', () => {
-    for (const room of socket.rooms) {
-      if (room !== socket.id) {
-        setTimeout(() => {
-          const userCount = io.sockets.adapter.rooms.get(room)?.size || 0;
-          io.to(room).emit('user-count', userCount);
-        }, 100); // Wait a bit for the room to update
-      }
+    const room = userRooms[socket.id];
+    if (room) {
+      socket.to(room).emit('user-left', socket.id);
+      setTimeout(() => {
+        const userCount = io.sockets.adapter.rooms.get(room)?.size || 0;
+        io.to(room).emit('user-count', userCount);
+      }, 100);
+      delete userRooms[socket.id];
     }
   });
 
