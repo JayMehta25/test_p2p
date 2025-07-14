d6d9a26a38f87ca168c3487cd04efb6b1a1595a5
Jay Mehta <jayjmehta251203@gmail.com>
2025-07-14 14:15:09 +0530

    server changes made

--- server.js
+++ server.js
@@ -13,11 +13,25 @@ io.on('connection', socket => {
     socket.join(room);
     console.log(`User ${socket.id} joined room: ${room}`);
     socket.to(room).emit('peer-joined');
+    // Emit user count to all in the room
+    const userCount = io.sockets.adapter.rooms.get(room)?.size || 0;
+    io.to(room).emit('user-count', userCount);
   });
 
   socket.on('signal', ({ room, data }) => {
     console.log(`Signal from ${socket.id} to room ${room}:`, data.sdp ? 'SDP' : 'ICE Candidate');
     socket.to(room).emit('signal', data);
+  });
+
+  socket.on('disconnecting', () => {
+    for (const room of socket.rooms) {
+      if (room !== socket.id) {
+        setTimeout(() => {
+          const userCount = io.sockets.adapter.rooms.get(room)?.size || 0;
+          io.to(room).emit('user-count', userCount);
+        }, 100); // Wait a bit for the room to update
+      }
+    }
   });
 
   socket.on('disconnect', () => {
